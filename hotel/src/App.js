5672e7779a9c65e7e76be0f11b204b70e82f0e49
Abhishek Patil <Abhishek.Patil@emtecinc.com>
2022-10-18 04:10:09 -0700

    Created basic project structure

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import AdminLogin from './component/login/AdminLogin';
+import UserLogin from './component/login/UserLogin';
+import Navbar from './component/navbar/Navbar';
+import Registration from './component/registration/Registration';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/userLogin' element={<UserLogin/>} ></Route>
+        <Route path='/adminLogin' element={<AdminLogin/>} ></Route>
+        <Route path='/registration' element={<Registration/>} ></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ hotel/src/component/login/UserLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserLogin = () => {
+  return (
+    <div>UserLogin
+        <h1>abhishek</h1>
+    </div>
+  )
+}
+
+export default UserLogin
